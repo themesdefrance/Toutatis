10f46f796defc80f69abd05b77ff6c3fef92c47e
Alex Borto <alex@themesdefrance.fr>
2015-01-23 01:35:09 +0100

    Title widget social

--- admin/widgets/social.php
+++ admin/widgets/social.php
@@ -7,12 +7,12 @@
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since 1.0
  */
-	
+
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class ToutatisSocial extends WP_Widget{
-	
+
 	/**
 	 * Social networks available
 	 *
@@ -32,7 +32,7 @@ class ToutatisSocial extends WP_Widget{
 		'dribbble'=>'social-dribbble',
 		'rss'=>'rss'
 	);
-	
+
 	/**
 	 * Error checking
 	 *
@@ -41,7 +41,7 @@ class ToutatisSocial extends WP_Widget{
 	 * @since 1.0
 	 */
 	private $error = false;
-	
+
 	/**
 	 * How many fields have to be displayed by default
 	 *
@@ -50,7 +50,7 @@ class ToutatisSocial extends WP_Widget{
 	 * @since 1.0
 	 */
 	private $hideAfter = 3;
-	
+
 	/**
 	 * Initializes the object instance
 	 *
@@ -64,7 +64,7 @@ class ToutatisSocial extends WP_Widget{
 			array('description'=>__('Display links to your social profiles.', 'toutatis'),)
 		);
 	}
-	
+
 	/**
 	 * Display the widget on the website
 	 *
@@ -77,29 +77,29 @@ class ToutatisSocial extends WP_Widget{
 	 */
 	public function widget($args, $instance){
 		echo $args['before_widget'];
-		
+
 		if (isset($instance['title']) && !empty($instance['title'])){
 			echo $args['before_title'].apply_filters('widget_title', $instance['title']).$args['after_title'];
 		}
-		
+
 		echo '<ul>';
-		
+
 		foreach ($this->reseaux as $reseau=>$icon){
 			if (isset($instance[$reseau]) && !empty($instance[$reseau])){
 				?>
 				<li>
 					<a href="<?php echo $instance[$reseau]; ?>" title="<?php echo $reseau; ?>" class="typcn typcn-<?php echo $icon; ?>" target="_blank">
-						
+
 					</a>
 				</li>
 				<?php
 			}
 		}
-		
+
 		echo '</ul>';
 		echo $args['after_widget'];
 	}
-	
+
 	/**
 	 * Display the widget form
 	 *
@@ -107,31 +107,31 @@ class ToutatisSocial extends WP_Widget{
      *
 	 * @since 1.0
 	 * @return void
-	 */	
+	 */
 	public function form($instance){
-		
-		$fields = array_merge(array(__('Title','toutatis')), array_keys($this->reseaux));
-		
+
+		$fields = array_merge(array('title'), array_keys($this->reseaux));
+
 		echo '<style>';
 		include 'widgets.css';
 		echo '</style>';
 		echo '<script>';
 		include 'widgets.js';
 		echo '</script>';
-		
+
 		if ($this->error){
 			echo '<div class="error">';
 			_e('Please enter a valid url', 'toutatis');
 			echo '</div>';
 		}
-		
+
 		// Check if hidden networks have any value
 		$open = '';
-		
+
 		foreach ($instance as $reseau){
 			if (in_array($reseau, array_slice($fields, $this->hideAfter+1))) $open = 'open';
 		}
-		
+
 		foreach ($fields as $count=>$field){
 			if ($count === $this->hideAfter+1):?>
 			<div>
@@ -143,19 +143,21 @@ class ToutatisSocial extends WP_Widget{
 			</div>
 			<div class="toutatissocial-toggle <?php echo $open; ?>">
 			<?php endif; ?>
-			
+
 			<?php $value = (isset($instance[$field])) ? $instance[$field] : ''; ?>
 			<p>
 				<label for="<?php echo $this->get_field_id($field); ?>">
-					<?php echo ucfirst(str_replace("-"," ",$field)).':'; ?>
-				</label> 
+
+					<?php echo ($field == 'title' ? __('Title','toutatis') : ucfirst(str_replace("-"," ",$field)) ) . ':'; ?>
+
+				</label>
 				<input class="widefat" id="<?php echo $this->get_field_id($field); ?>" name="<?php echo $this->get_field_name($field); ?>" type="url" value="<?php echo esc_attr($value); ?>" />
 			</p>
 			<?php
 		}
 		echo '</div>';//closing more div
 	}
-	
+
 	/**
 	 * Update the widget settings
 	 *
@@ -166,14 +168,14 @@ class ToutatisSocial extends WP_Widget{
 	 * @return array Settings to save or bool false to cancel saving.
 	 */
 	public function update($new_instance, $old_instance){
-		
+
 		foreach ($this->reseaux as $reseau){
 			if (isset($new_instance[$reseau]) && !empty($new_instance[$reseau]) && !filter_var($new_instance[$reseau], FILTER_VALIDATE_URL)){
 				$new_instance[$reseau] = $old_instance[$reseau];
 				$this->error = true;
 			}
 		}
-		
+
 		return $new_instance;
 	}
 }
